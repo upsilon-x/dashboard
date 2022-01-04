260f2b866d51e82755ff22a464954b13118fee6d
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-01-04 00:07:23 -0500

    Added template project switcher

--- @jumbo/components/AppLayout/partials/Header/index.js
+++ @jumbo/components/AppLayout/partials/Header/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SidebarToggleHandler from '../../../../../@coremat/CmtLayouts/Vertical/SidebarToggleHandler';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Box, InputBase } from '@material-ui/core';
+import { Box, InputBase, IconButton, Typography } from '@material-ui/core';
 import { alpha } from '@material-ui/core/styles';
-import LanguageSwitcher from '../LanguageSwitcher';
+import CmtDropdownMenu from '../../../../../@coremat/CmtDropdownMenu';
+import CmtImage from '../../../../../@coremat/CmtImage';
+import SettingsIcon from '@material-ui/icons/Settings';
+import PersonIcon from '@material-ui/icons/Person';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+//import LanguageSwitcher from '../LanguageSwitcher';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+/*
 import SearchIcon from '@material-ui/icons/Search';
 import AppsMenu from './AppsMenu';
 import HeaderNotifications from './HeaderNotifications';
@@ -12,6 +18,7 @@ import HeaderMessages from './HeaderMessages';
 import Hidden from '@material-ui/core/Hidden';
 import Logo from '../Logo';
 import SearchPopover from '../SearchPopover';
+*/
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -78,15 +85,54 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// TODO: Add to context + do a query request if context is null
+// TODO: Put the context switcher in its own component
+const actionsList = [
+  {
+    icon: <CmtImage src="https://play-lh.googleusercontent.com/10axL9ZMum2LZmCsVutZwvwfx0bkYhB-G7c12Qvl1xDexMYxcqwILCYNgnzzcSDbLrAw=s180-rw"
+      height="30px" width="30px" className="mr-2" />,
+    label: 'Project 1',
+    image: "https://play-lh.googleusercontent.com/10axL9ZMum2LZmCsVutZwvwfx0bkYhB-G7c12Qvl1xDexMYxcqwILCYNgnzzcSDbLrAw=s180-rw"
+  },
+  {
+    icon: <CmtImage src="https://play-lh.googleusercontent.com/-meRETSTUS8DBtnim75eGwlTPncfiUpR5zAiSl3hu5NnuETVmYA4Fk-vIUBVWdd-ynw=s180-rw"
+      height="30px" width="30px" className="mr-2" />,
+    label: 'Project 2',
+    image: "https://play-lh.googleusercontent.com/-meRETSTUS8DBtnim75eGwlTPncfiUpR5zAiSl3hu5NnuETVmYA4Fk-vIUBVWdd-ynw=s180-rw"
+  },
+];
+
 const Header = () => {
   const classes = useStyles();
+
+  const [activeOption, setActiveOption] = useState(actionsList[0]);
+
+  const onItemClick = (option) => {
+    setActiveOption({ label: option.label, image: option.image });
+  };
 
   return (
     <Toolbar className={classes.root}>
       <SidebarToggleHandler edge="start" color="inherit" aria-label="menu" />
+      <Box display="flex" flexDirection='row' mt='unset'>
+        <CmtImage
+          src={activeOption.image}
+          height="30px"
+          width="30px"
+          className="mr-2"
+        />
+        <Typography size="md" className="mr-1">{activeOption.label}</Typography>
+        <CmtDropdownMenu
+          onItemClick={onItemClick}
+          TriggerComponent={<KeyboardArrowDownIcon />}
+          items={actionsList}
+        />
+      </Box>
+      <Box flex={1} />
+
+      {/*
       <Logo ml={2} color="white" />
       <Box flex={1} />
-      {/*
       <Hidden smDown>
         <Box pr={3} className={classes.searchRoot}>
           <InputBase placeholder={'Search here...'} inputProps={{ 'aria-label': 'search' }} />
