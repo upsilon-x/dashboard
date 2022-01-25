e3683608dfaa54545cc06e0c535bc8b7bcefa1c5
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-01-25 16:36:26 -0600

    Connected release page to database

--- modules/Pages/Releases/create.js
+++ modules/Pages/Releases/create.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import GridContainer from '../../../@jumbo/components/GridContainer';
 import PageContainer from '../../../@jumbo/components/PageComponents/layouts/PageContainer';
 import IntlMessages from '../../../@jumbo/utils/IntlMessages';
 import Grid from '@material-ui/core/Grid';
-import { Button, Card, InputLabel, FormControl } from '@material-ui/core';
-import { Input } from '@material-ui/core';
+import { Button, Card } from '@material-ui/core';
 import { useForm } from "react-hook-form";
 import ValidatedInput from "../../Components/Form/ValidatedInput";
+import DappContext from "../../Context/DappContext";
+import ENV_VAR from "../../../ENV_VAR.json";
+import { useEthers } from '@usedapp/core';
+import firebase from 'firebase';
 
 const breadcrumbs = [
   { label: 'Home', link: '/' },
@@ -14,14 +17,49 @@ const breadcrumbs = [
   { label: 'Create', isActive: true },
 ];
 
+const functionsURL = ENV_VAR[process.env.NODE_ENV].functions;
+
 const Releases = () => {
   const [submitting, setSubmitting] = useState(false);
+  const { account, chainId } = useEthers();
+  const { selectedProject, projects } = useContext(DappContext);
   const { register, handleSubmit, formState: { errors } } = useForm();
+
   const onSubmit = function (data) {
     setSubmitting(true);
+
     // 1. upload
-    console.log(data);
-    // 2. do API request
+    const rand = Math.floor(Math.random() * 1000000000000);
+    firebase.storage()
+      .ref(`/releases/${account}/${projects[selectedProject].nftId}/${rand}-${data.version}/`)
+      .put(data.webgl[0])
+      .then((snapshot) => {
+        console.log('One success:', data.webgl[0])
+        return snapshot.ref.getDownloadURL();
+      })
+      .catch((error) => {
+        console.log('One failed:', data.webgl[0], error.message)
+      })
+
+      // 2. do API request
+      .then(url => {
+        console.log(data);
+        data.webgl = url;
+
+        firebase.auth().currentUser.getIdToken()
+          .then(async token => {
+            return fetch(`${functionsURL}/createRelease`, {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': token
+              },
+              body: JSON.stringify(data)
+            })
+          })
+          .then(res => { window.location.href = "/releases"; });
+      });
   }
 
   return (
@@ -33,14 +71,20 @@ const Releases = () => {
               <div>Please wait while the release is being uploaded. Do not leave this page.</div>
               :
               <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="hidden" id="chainId" {...register("chainId")} value={chainId} />
+                <input type="hidden" id="nftId" {...register("nftId")}
+                  value={projects[selectedProject].nftId}
+                />
                 <ValidatedInput errors={errors} register={register}
                   inputId="version" inputType="text"
                   label="*Release Version:"
-                  validation={{ required: true, 
-                    validate: value => ( /[0-9.]/.test(value) ? true : "Must be in the form 1.0.1" )}}
+                  validation={{
+                    required: true,
+                    validate: value => (/[0-9.]/.test(value) ? true : "Must be in the form 1.0.1")
+                  }}
                 />
                 <ValidatedInput errors={errors} register={register}
-                  inputId="releasefile" inputType="file"
+                  inputId="webgl" inputType="file"
                   label="*Release WebGL File:"
                   validation={{ required: true }}
                 />
--- modules/Pages/Releases/index.js
+++ modules/Pages/Releases/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import GridContainer from '../../../@jumbo/components/GridContainer';
 import PageContainer from '../../../@jumbo/components/PageComponents/layouts/PageContainer';
 import IntlMessages from '../../../@jumbo/utils/IntlMessages';
 import Grid from '@material-ui/core/Grid';
 import { Button, Card, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 import Link from 'next/link';
+import DappContext from "../../Context/DappContext";
+import { useEthers } from '@usedapp/core'
+import ENV_VAR from "../../../ENV_VAR.json";
+
+const functionsURL = ENV_VAR[process.env.NODE_ENV].functions;
 
 const breadcrumbs = [
   { label: 'Home', link: '/' },
@@ -13,42 +18,74 @@ const breadcrumbs = [
 
 const Releases = () => {
 
-  // TODO: firebase request
-  const rows = [
+  const { projects, selectedProject } = useContext(DappContext);
+
+  /*
+  const releases = [
     { "id": 9, "version": "1.0.3", "status": "Live" },
     { "id": 7, "version": "0.8.0", "status": "Archived" },
     { "id": 6, "version": "0.7.2", "status": "Archived" },
     { "id": 5, "version": "0.7.1", "status": "Archived" },
     { "id": 2, "version": "0.6.0", "status": "Archived" },
-  ]
+  ];
+  */
+
+  const { chainId } = useEthers();
+
+  const [releases, setReleases] = useState(null);
+  if (releases == null && projects != null) {
+    setReleases(false);
+    fetch(`${functionsURL}/getReleases`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nftId: projects[selectedProject].nftId,
+        chainId: chainId
+      })
+    })
+    .then((response) => response.json())
+    .then(res => {
+      console.log(res);
+      setReleases(res);
+    })
+  }
+
+
 
   return (
     <PageContainer heading={<IntlMessages id="pages.releasesPage" />} breadcrumbs={breadcrumbs}>
       <GridContainer>
         <Grid item xs={12} md={8}>
-          {/* NOTE: Potentially use MUI data grid? */}
-          <TableContainer>
-            <Table aria-label="release-table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Release Id</TableCell>
-                  <TableCell>Version</TableCell>
-                  <TableCell align="right">Status</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow key={row.id}>
-                    <TableCell component="th" scope="row">
-                      {row.id}
-                    </TableCell>
-                    <TableCell>v{row.version}</TableCell>
-                    <TableCell align="right">{row.status}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          {/* NOTE: Potentially use MUI data grid? */
+            releases === null || releases === false ?
+              <div>Hold on. Getting releases...</div>
+              :
+              <TableContainer>
+                <Table aria-label="release-table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Release Id</TableCell>
+                      <TableCell>Version</TableCell>
+                      <TableCell align="right">Status</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {releases.map((row) => (
+                      <TableRow key={row.releaseId}>
+                        <TableCell component="th" scope="row">
+                          {row.releaseId}
+                        </TableCell>
+                        <TableCell>v{row.version}</TableCell>
+                        <TableCell align="right">{row.status}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+          }
         </Grid>
         <Grid item xs={12} md={4}>
           <Card style={{ padding: "12px 12px" }}>
